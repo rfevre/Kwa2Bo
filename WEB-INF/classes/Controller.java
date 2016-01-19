1ab27883a3ab5f314fdea2d9dea8bd22ac4fc0d1
Rémy Fèvre <r.fevre184@gmail.com>
2016-01-19 10:57:23 +0100

    Servlet Inscription

---
+++ WEB-INF/classes/Controller.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import java.sql.*;
+import java.util.Properties;
+
+@WebServlet("/servlet/Controller")
+public class Controller extends HttpServlet {
+  public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+    RequestDispatcher dispatcher = null;
+    String param = request.getParameter("go");
+    if (param == null) throw new ServletException(" Missing parameter in Controller.");
+    else if (param.equals("test")) dispatcher = getServletContext().getNamedDispatcher("Test");
+    else if (param.equals("inscription")) dispatcher = getServletContext().getNamedDispatcher("Inscription");
+    else if (param.equals("identification")) dispatcher = getServletContext().getNamedDispatcher("Identification");
+    else throw new ServletException(" Improper parameter passed to Controller.");
+    /*check for a null dispatcher, then dispatch the request to the correct URL*/
+    if (dispatcher != null) dispatcher.forward(request,response);
+    else throw new ServletException(" Controller received a null dispatcher.");
+  }
+}
--- login.jsp
+++ login.jsp
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-	<meta charset="utf-8"/>
-	<link rel="stylesheet" type="text/css" href="./css/style.css"/>
-	<title>Titre de la page</title>
+<title>Page de login - Kwa2Bo</title>
+<link rel="stylesheet" href="${pageContext.request.contextPath}/css/style.css" type="text/css">
 </head>
 <body>
 	<header>
@@ -15,7 +14,7 @@
 					<input type = "password" name="j_password" placeholder="Mot de passe"/>
 					<input type = "submit" value="Connexion" class="btn btn-primary"/>
 			</form>
-			<a href="#">Mot de passe oublié ?</a>
+			<a href="#">${message} Mot de passe oublié ? </a>
 		</div>
 	</header>
 	<section>
