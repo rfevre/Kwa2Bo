ebb8771b9c8b62bb0f6ef43fcdf46b65f6bd9533
Remy <fevrer@localhost.localdomain>
2016-01-21 22:40:42 +0100

    Test Mail + MaJ fichier web.xml

--- WEB-INF/classes/TestMail.java
+++ WEB-INF/classes/TestMail.java
@@ -6,32 +6,56 @@ import java.sql.*;
 import javax.sql.*;
 import java.util.Properties;
 import javax.naming.*;
-import javax.mail.Authenticator;
+import mail.SendMail;
+
 import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+
 
 @WebServlet("/servlet/TestMail")
 public class TestMail extends HttpServlet {
   public void service(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+    PrintWriter out = response.getWriter();
+    //SendMail mail = new SendMail();
+
+
+    final String username = "dashdu59@gmail.com";
+    final String password = "lapin310";
+
+    Properties props = new Properties();
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.smtp.host", "smtp.gmail.com");
+    props.put("mail.smtp.port", "587");
+
+    Session session = Session.getInstance(props,
+      new javax.mail.Authenticator() {
+        protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(username, password);
+        }
+      });
+
     try {
-      Context initCtx = new InitialContext();
-      Context envCtx = (Context) initCtx.lookup("java:comp/env");
-      javax.mail.Session sess = (javax.mail.Session) envCtx.lookup("mail/Session");
-      Message message = new MimeMessage(sess);
-      message.setFrom(new InternetAddress("dashdu59@gmail.com"));
-      InternetAddress to[] = new InternetAddress[1];
-      to[0] = new InternetAddress("r.fevre184@gmail.com");
-      message.setRecipients(Message.RecipientType.TO, to);
-      message.setSubject("test");
-      message.setContent("taratata taratata", "text/plain");
-      Transport.send(message);
-    }catch(Exception e){
-      throw new ServletException("Y'a un probléme quelque part.");
+
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("dashdu59@gmail.com"));
+        message.setRecipients(Message.RecipientType.TO,
+            InternetAddress.parse("r.fevre184@gmail.com"));
+        message.setSubject("A testing mail header !!!");
+        message.setText("Dear Mail Crawler,"
+            + "\n\n No spam to my email, please!");
+
+        Transport.send(message);
+
+        out.println("Done");
+
+    } catch (MessagingException e) {
+        out.println(e);
     }
   }
 }
