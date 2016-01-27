cf2c675d2fbfb83c5e3d6502099c2365863122a4
Rémy Fèvre <r.fevre184@gmail.com>
2016-01-27 17:43:50 +0100

    MaJ mineur

--- WEB-INF/classes/TestMail.java
+++ WEB-INF/classes/TestMail.java
@@ -19,24 +19,21 @@ import javax.mail.internet.MimeMessage;
 @WebServlet("/servlet/TestMail")
 public class TestMail extends HttpServlet {
   public void service(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
-    //SendMail mail = new SendMail()
-    PrintWriter out = response.getWriter();
     Context initCtx;
- 		try {
- 			initCtx = new InitialContext();
- 			Context envCtx = (Context) initCtx.lookup("java:comp/env");
- 			javax.mail.Session sess = (javax.mail.Session) envCtx.lookup("mail/Session");
- 			Message message = new MimeMessage(sess);
- 			message.setFrom(new InternetAddress("r.fevre184@gmail.com"));
- 			InternetAddress to[] = new InternetAddress[1];
- 			to[0] = new InternetAddress("t.ferro184@gmail.com");
- 			message.setRecipients(Message.RecipientType.TO, to);
- 			message.setSubject("Inscription sur Kwa2Bo !");
- 			message.setContent("The best messagerie Ever ! for sure !", "text/plain");
- 			Transport.send(message);
- 		} catch (Exception e) {
-      out.println(e);
-      //throw new ServletException("Problem "+e);
- 		}
+    try {
+      initCtx = new InitialContext();
+      Context envCtx = (Context) initCtx.lookup("java:comp/env");
+      javax.mail.Session sess = (javax.mail.Session) envCtx.lookup("mail/Session");
+      Message message = new MimeMessage(sess);
+      message.setFrom(new InternetAddress("r.fevre184@gmail.com"));
+      InternetAddress to[] = new InternetAddress[1];
+      to[0] = new InternetAddress("t.ferro184@gmail.com");
+      message.setRecipients(Message.RecipientType.TO, to);
+      message.setSubject("Inscription sur Kwa2Bo !");
+      message.setContent("The best messagerie Ever ! for sure !", "text/plain");
+      Transport.send(message);
+    } catch (Exception e) {
+      throw new ServletException("Error : "+e);
+    }
   }
 }
