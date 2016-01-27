f636361b14deb9d6659ffffbe6d2220485b99b48
Rémy Fèvre <r.fevre184@gmail.com>
2016-01-27 17:29:14 +0100

    Preparation Envoie Mail

---
+++ WEB-INF/classes/TestMail.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import java.sql.*;
+import javax.sql.*;
+import java.util.Properties;
+import javax.naming.*;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+@WebServlet("/servlet/TestMail")
+public class TestMail extends HttpServlet {
+  public void service(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+    //SendMail mail = new SendMail()
+    PrintWriter out = response.getWriter();
+    Context initCtx;
+ 		try {
+ 			initCtx = new InitialContext();
+ 			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+ 			javax.mail.Session sess = (javax.mail.Session) envCtx.lookup("mail/Session");
+ 			Message message = new MimeMessage(sess);
+ 			message.setFrom(new InternetAddress("r.fevre184@gmail.com"));
+ 			InternetAddress to[] = new InternetAddress[1];
+ 			to[0] = new InternetAddress("t.ferro184@gmail.com");
+ 			message.setRecipients(Message.RecipientType.TO, to);
+ 			message.setSubject("Inscription sur Kwa2Bo !");
+ 			message.setContent("The best messagerie Ever ! for sure !", "text/plain");
+ 			Transport.send(message);
+ 		} catch (Exception e) {
+      out.println(e);
+      //throw new ServletException("Problem "+e);
+ 		}
+  }
+}
